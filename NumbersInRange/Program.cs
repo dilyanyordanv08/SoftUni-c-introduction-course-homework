1be10339166ecc45a327b2d88fec675335810756
dilyanyordanv08 <dilyan.yordanov08@gmail.com>
2016-05-09 16:29:53 +0300

    Add files via upload

---
+++ NumbersInRange/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number in the range [1...100]: ");
+            int n = int.Parse(Console.ReadLine());
+            
+
+            if (n >= 1 && n <= 100)
+            {
+                
+                Console.WriteLine("The number is: {0}", n);
+            }
+            else
+            {
+                
+                while (n < 1 || n > 100)
+                {
+
+                 Console.WriteLine("Invalid number!");
+                    Console.Write("Enter a number in the range [1...100]: ");
+                    n = int.Parse(Console.ReadLine());
+
+                }
+                Console.WriteLine("The number is: {0}", n);
+            }
+        }
+    }
+}
---
+++ PowersOfTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowersOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var num = 1;
+            for (int i = 0; i < n + 1; i++)
+            {
+              
+                Console.WriteLine(num);
+                num = num * 2;
+
+            }
+        }
+    }
+}
