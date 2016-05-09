2671b76265947af6456921db7700196c56601476
dilyanyordanv08 <dilyan.yordanov08@gmail.com>
2016-05-09 16:35:19 +0300

    Add files via upload

---
+++ OddEvenPosition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sumEven = 0.00;
+            var maxEven = 0.00;
+            var minEven = 0.00;
+            var minOdd = 0.00;
+            var maxOdd = 0.00;
+            var sumOdd = 0.00;
+
+          var counter1 = 100000000;
+          var  counter2 = -10000000;
+
+            for (int i = 1; (i > counter2) && (i < counter1); i++)
+            {
+                var nums = double.Parse(Console.ReadLine());
+                if (nums % 2 == 0)
+                {
+                    sumEven = sumEven + nums;
+                    Console.WriteLine(sumEven);
+                }
+            }
+              
+           
+
+            
+
+    }
+    }
+}
---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int num > 1; num < n ; num++)
+            {
+                sum = sum + num;
+            }
+
+            
+        }
+    }
+}
