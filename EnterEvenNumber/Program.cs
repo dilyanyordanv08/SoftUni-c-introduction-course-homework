17ae4c978a3ecdea5681982d3efa3e8be18ca50b
dilyanyordanv08 <dilyan.yordanov08@gmail.com>
2016-05-09 16:27:00 +0300

    Add files via upload

---
+++ EnterEvenNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                try
+                {
+                    if (n % == 0)
+                    {
+                     catch (Exception);
+                }
+                Console.WriteLine("Odd number");
+                n = int.Parse(Console.ReadLine());
+
+            
+            }
+            Console.WriteLine("Invalid number" );
+
+        }
+    }
+}
