d3133d7ffeab8077e80063c2252d36c3d0905bdf
dilyanyordanv08 <dilyan.yordanov08@gmail.com>
2016-05-09 16:26:35 +0300

    Add files via upload

---
+++ EvenPowersOfTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenPowersOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int multiplier = 1;
+
+
+            for (int cnt = 0; cnt < n + 1; cnt += 2)
+            {
+                Console.WriteLine(multiplier);
+                multiplier = multiplier * 2 * 2;
+
+            }
+        }
+    }
+}
