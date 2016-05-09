52bb992a6bfdf68d1f36b5e25e18f128f6823fb8
dilyanyordanv08 <dilyan.yordanov08@gmail.com>
2016-05-09 16:21:11 +0300

    Add files via upload

---
+++ CheckPrime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+
+            if (n < 2)
+            {
+                isPrime = false;
+            }
+            for (int cnt = 2; cnt <= Math.Sqrt(n); cnt++)
+            {
+                if (n % cnt == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+                
+            
+        }
+    }
+}
